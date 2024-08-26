ba86f26502cb89b87e1b8d4bd9f9f01c4ba4f83a
gyu won oh <ogw0627@gmail.com>
2024-08-26 18:18:22 +0900

    rest api/ajax 댓글 게시판 만들기

---
+++ Spring/work/22boardAjaxReply2/src/main/java/com/human/dto/replyDto.java
@@ -0,0 +1,96 @@
+package com.human.dto;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class replyDto {
+	private int rId;
+	private int bId;
+	private String rContent;
+	private String rName;
+	@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+	private Date rWriteTime;
+	@DateTimeFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+	private Date rUpdateTime;
+	private String rEtc;
+	private int rGroup;
+	private int rStep;
+	private int rIndent;
+	private String rDelete;
+	public int getrId() {
+		return rId;
+	}
+	public void setrId(int rId) {
+		this.rId = rId;
+	}
+	public int getbId() {
+		return bId;
+	}
+	public void setbId(int bId) {
+		this.bId = bId;
+	}
+	public String getrContent() {
+		return rContent;
+	}
+	public void setrContent(String rContent) {
+		this.rContent = rContent;
+	}
+	public String getrName() {
+		return rName;
+	}
+	public void setrName(String rName) {
+		this.rName = rName;
+	}
+	public Date getrWriteTime() {
+		return rWriteTime;
+	}
+	public void setrWriteTime(Date rWriteTime) {
+		this.rWriteTime = rWriteTime;
+	}
+	public Date getrUpdateTime() {
+		return rUpdateTime;
+	}
+	public void setrUpdateTime(Date rUpdateTime) {
+		this.rUpdateTime = rUpdateTime;
+	}
+	public String getrEtc() {
+		return rEtc;
+	}
+	public void setrEtc(String rEtc) {
+		this.rEtc = rEtc;
+	}
+	public int getrGroup() {
+		return rGroup;
+	}
+	public void setrGroup(int rGroup) {
+		this.rGroup = rGroup;
+	}
+	public int getrStep() {
+		return rStep;
+	}
+	public void setrStep(int rStep) {
+		this.rStep = rStep;
+	}
+	public int getrIndent() {
+		return rIndent;
+	}
+	public void setrIndent(int rIndent) {
+		this.rIndent = rIndent;
+	}
+	public String getrDelete() {
+		return rDelete;
+	}
+	public void setrDelete(String rDelete) {
+		this.rDelete = rDelete;
+	}
+	
+	@Override
+	public String toString() {
+		return "replyDto [rId=" + rId + ", bId=" + bId + ", rContent=" + rContent + ", rName=" + rName + ", rWriteTime="
+				+ rWriteTime + ", rUpdateTime=" + rUpdateTime + ", rEtc=" + rEtc + ", rGroup=" + rGroup + ", rStep="
+				+ rStep + ", rIndent=" + rIndent + ", rDelete=" + rDelete + "]";
+	}
+	
+	
+}
